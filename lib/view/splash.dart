a7f957a87601bb06a3742cddce7ba7f9b1b4bdba
Shubham Khanal <shubhamkhanal593@gmail.com>
2025-05-17 22:25:58 +0545

    changes in import and others

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,8 +1,6 @@
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:jeevandaan/view/boarding.dart';
-import 'package:jeevandaan/view/login.dart';
 import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
