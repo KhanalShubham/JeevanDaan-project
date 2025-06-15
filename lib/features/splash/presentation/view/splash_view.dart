30b32a9822a6752e794513f1a82d1c269f32b659
Shubham Khanal <shubhamkhanal593@gmail.com>
2025-06-15 22:57:47 +0545

    BlocProvider for SplashViewModel

---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jeevandaan/features/splash/presentation/view_model/splash_view_model.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Call the ViewModel to handle navigation after 2 seconds
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<SplashViewModel>().init(context);
+    });
+
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          // Logo image
+          Image.asset(
+            'assets/images/logo.png',
+            width: 120,
+            height: 120,
+            fit: BoxFit.contain,
+          ),
+          // App name
+          const Padding(
+            padding: EdgeInsets.only(top: 140), // Adjust to position below logo
+            child: Text(
+              'Jeevan Daan',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          // Loading animation
+          Padding(
+            padding: const EdgeInsets.only(top: 240), // Adjust to position below app name
+            child: Lottie.asset(
+              'assets/lottie/loading.json',
+              width: 150,
+              height: 150,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
