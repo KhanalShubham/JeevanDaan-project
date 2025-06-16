6e3e07677e47b83697c3b18c6e289af4d27635a7
Shubham Khanal <shubhamkhanal593@gmail.com>
2025-06-16 23:00:27 +0545

    centralized hice box and retrival in core-> network

--- lib/core/network/hive_services.dart
+++ lib/core/network/hive_services.dart
@@ -8,4 +8,21 @@ class HiveServices {
     var path='${directory.path}/jeevandaan.db';
     Hive.init(path);
   }
+
+  
+  // Future<UserHiveModel?>Login(String email, String password)async{
+  //   var box=await Hive.openBox<UserHiveModel>(
+  //     HiveTableConstant.userBoxName,
+  //   );
+  //   var user= box.values.firstWhere(
+  //     (element)=>element.email==email && element.password==password,
+  //     orElse: ()=>throw Exception("invalid username or password"),
+  //   );
+  //   box.close();
+  //   return user;
+  // }
+
+  static Future<Box>openBox(String userBoxName)async{
+    return await Hive.openBox(userBoxName);
+  }
 }
